dfc801d336ae06248ef98ca6e15e5938cb0f49ad
EnableKorea <EnableKorea@Valentins-MacBook-Pro.local>
2017-03-10 18:03:29 +0900

    Simple routes test

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -27,34 +27,34 @@ $response = new \Http\HttpResponse;
 
 
 
-$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
-  $routes = include('Routes.php');
-  foreach ($routes as $route) {
-    $r->addRoute($route[0], $route[1], $route[2]);
-  }
-};
 
-$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/hello-world', function () {
+        echo 'Hello World';
+    });
+    $r->addRoute('GET', '/another-route', function () {
+        echo 'This works too';
+    });
+});
 
 $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
 switch ($routeInfo[0]) {
-  case \FastRoute\Dispatcher::NOT_FOUND:
-    $response->setContent('404 - Page not found');
-    $response->setStatusCode(404);
-    break;
-  case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-    $response->setContent('405 - Method not allowed');
-    $response->setStatusCode(405);
-    break;
-  case \FastRoute\Dispatcher::FOUND:
-    $className = $routeInfo[1][0];
-    $method = $routeInfo[1][1];
-    $vars = $routeInfo[2];
-
-    $class = new $className;
-    $class->$method($vars);
-    break;
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
 }
+
+
 
 
 
