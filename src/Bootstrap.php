ef8a2204b85cd8c2e7586ac4dcf018b380c28b2e
EnableKorea <EnableKorea@Valentins-MacBook-Pro.local>
2017-03-13 09:48:14 +0900

    Add dependencies injection

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,37 +20,43 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 
 
 
 
-$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/hello-world', function () {
-        echo 'Hello World';
-    });
-    $r->addRoute('GET', '/another-route', function () {
-        echo 'This works too';
-    });
-});
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+  $routes = include('Routes.php');
+  foreach ($routes as $route) {
+    $r->addRoute($route[0], $route[1], $route[2]);
+  }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
 
 $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
 switch ($routeInfo[0]) {
-    case \FastRoute\Dispatcher::NOT_FOUND:
-        $response->setContent('404 - Page not found');
-        $response->setStatusCode(404);
-        break;
-    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $response->setContent('405 - Method not allowed');
-        $response->setStatusCode(405);
-        break;
-    case \FastRoute\Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-        call_user_func($handler, $vars);
-        break;
+  case \FastRoute\Dispatcher::NOT_FOUND:
+    $response->setContent('404 - Page not found');
+    $response->setStatusCode(404);
+  break;
+  case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+    $response->setContent('405 - Method not allowed');
+    $response->setStatusCode(405);
+  break;
+  case \FastRoute\Dispatcher::FOUND:
+    $className = $routeInfo[1][0];
+    $method = $routeInfo[1][1];
+    $vars = $routeInfo[2];
+
+    $class = $injector->make($className);
+    $class->$method($vars);
+  break;
 }
 
 
